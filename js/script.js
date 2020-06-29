8055d9d54b632c3ac881e7be8937b64f49782825
Rabin Gharti Magar <rabin_mgr@live.com>
2020-06-29 13:24:38 +0100

    modified files

--- js/script.js
+++ js/script.js
@@ -1,4 +1,7 @@
 $().ready(function () {
+  $(".btn").attr("title", "Click here to download");
+  $(".social a").attr("target", "_blank");
+
   $(".btn").on("click", function () {
     alert("Your download will begin shortly ...");
   });
